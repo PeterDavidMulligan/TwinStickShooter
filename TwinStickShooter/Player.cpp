3df6ec9595ff9d51bd2301996ca3d6951f2a0387
Peter Mulligan <Peter Mulligan>
2016-03-27 00:12:24 +0000

    Refactored Player functionality into a single class

--- TwinStickShooter/Player.cpp
+++ TwinStickShooter/Player.cpp
@@ -14,12 +14,12 @@
 // Constructor/Destructor
 ////////////////////////////////////////////////////////////
 goo::Player::Player(goo::AssetManager& assets) :
-goo::RotatingSprite(assets)
+m_assets(assets)
 {
 	initialise();
 }
 goo::Player::Player(sf::Vector2f position, goo::AssetManager& assets) :
-goo::RotatingSprite(assets)
+m_assets(assets)
 {
 	m_sprite.setPosition(position);
 	initialise();
@@ -95,5 +95,127 @@ void goo::Player::input()
 void goo::Player::update(sf::Time elapsedTime)
 {
 	input();
-	goo::RotatingSprite::update(elapsedTime);
+
+	//Check the velocity is powerful enough to move the sprite
+	if ((m_velocity.x < m_velocityDeadZone && m_velocity.x > -m_velocityDeadZone) &&
+		(m_velocity.y < m_velocityDeadZone && m_velocity.y > -m_velocityDeadZone))
+	{
+		m_velocity = sf::Vector2f(0, 0);
+	}
+	//move the sprite
+	m_sprite.move(m_velocity.x * m_speed * m_speedModifier * elapsedTime.asSeconds(),
+		m_velocity.y * m_speed * m_speedModifier * elapsedTime.asSeconds());
+
+	rotateToHeading();
+}
+
+////////////////////////////////////////////////////////////
+///	\brief Uses the current RenderWindow to draw elements
+///
+/// \param window Current RenderWindow
+///
+////////////////////////////////////////////////////////////
+void goo::Player::draw(sf::RenderWindow& window)
+{
+	window.draw(m_sprite);
+
+}
+
+////////////////////////////////////////////////////////////
+/// \brief Calculates the direction in degrees to a given
+/// target and then sets the Tower's heading to that value
+///
+/// \param target The sf::Vector2f that you want the Tower
+/// to face
+///
+////////////////////////////////////////////////////////////
+void goo::Player::setFacing(const sf::Vector2f target)
+{
+	//Set target offset
+	sf::Vector2f offset(target.y - m_sprite.getPosition().y, target.x - m_sprite.getPosition().x);
+	// This is the opposite of how normal geometry works - we must multiply the y component
+	//by - 1 when doing angle calculation.
+	float degrees = atan2(offset.y, offset.x) / acos(-1) * 180 + 180;
+	m_heading = static_cast<int>(degrees + 90) % 360;
+}
+
+////////////////////////////////////////////////////////////
+/// \brief Rotates the tower the shortest direction to face
+/// the Tower's heading value
+///
+////////////////////////////////////////////////////////////
+void goo::Player::rotateToHeading()
+{
+	int degrees = m_sprite.getRotation();
+	if (degrees != m_heading)//If it's not looking where it should be...
+	{
+		int rotation = m_heading - degrees % 360;//Check how far it has left to go
+		int signOfRotation = std::copysign(1.0f, rotation); //Check if that's shorter clockwise or not
+		int rotationSpeed = m_rotationSpeed;	//Set rotation speed to towers speed
+		if (std::abs(rotation) <= rotationSpeed)
+		{
+			//If the tower is leass than a turn away,
+			//reduce the turn speed to stop 'flip/flop'
+			rotationSpeed = 1;
+		}
+		if (std::abs(rotation) < 180)
+		{
+			m_sprite.rotate(signOfRotation * rotationSpeed);
+		}
+		else // degrees >= 180
+		{
+			m_sprite.rotate(-1.0f * signOfRotation * rotationSpeed);
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////
+// Getter Functions
+////////////////////////////////////////////////////////////
+sf::Vector2f goo::Player::getPosition()
+{
+	return m_sprite.getPosition();
+}
+float goo::Player::getRotationSpeed()
+{
+	return m_rotationSpeed;
+}
+float goo::Player::getHeading()
+{
+	return m_heading;
+}
+sf::Vector2f goo::Player::getVelocity()
+{
+	return m_velocity;
+}
+float goo::Player::getSpeed()
+{
+	return m_speed;
+}
+float goo::Player::getFriction()
+{
+	return m_speedModifier;
+}
+////////////////////////////////////////////////////////////
+// Setter Functions
+////////////////////////////////////////////////////////////
+void goo::Player::setPosition(sf::Vector2f position)
+{
+	m_sprite.setPosition(position);
+}
+void goo::Player::setRotationSpeed(float speed)
+{
+	m_rotationSpeed = speed;
+}
+void goo::Player::setVelocity(sf::Vector2f velocity)
+{
+	m_velocity = velocity;
+}
+void goo::Player::setSpeed(float speed)
+{
+	m_speed = speed;
+}
+void goo::Player::setFriction(float friction)
+{
+	m_speedModifier = friction;
 }
--- TwinStickShooter/Player.h
+++ TwinStickShooter/Player.h
@@ -10,7 +10,6 @@
 // Headers
 ////////////////////////////////////////////////////////////
 #include "AssetManager.h"
-#include "RotatingSprite.h"
 #include "..\Gamepad.h"
 
 ////////////////////////////////////////////////////////////
@@ -19,13 +18,28 @@
 ////////////////////////////////////////////////////////////
 namespace goo
 {
-	class Player : public goo::RotatingSprite
+	class Player
 	{
 	private:
 		////////////////////////////////////////////////////////////
 		// Member Variables
 		////////////////////////////////////////////////////////////
 		int m_analogDeadzone;
+		goo::AssetManager& m_assets;
+		sf::Sprite m_sprite;
+		sf::Vector2f m_velocity;
+		float m_velocityDeadZone;
+		float m_speed;
+		float m_speedModifier;
+
+		float m_rotationSpeed;
+		float m_heading;
+
+		////////////////////////////////////////////////////////////
+		// Private Functions
+		////////////////////////////////////////////////////////////
+		void setFacing(const sf::Vector2f target);
+		void rotateToHeading();
 
 	public:
 		////////////////////////////////////////////////////////////
@@ -40,7 +54,23 @@ namespace goo
 		////////////////////////////////////////////////////////////
 		void initialise();
 		void input();
+		void draw(sf::RenderWindow& window);
 		void update(sf::Time elapsedTime);
+
+		sf::Vector2f getVelocity();
+		float getSpeed();
+		float getFriction();
+
+		void setVelocity(sf::Vector2f velocity);
+		void setSpeed(float speed);
+		void setFriction(float friction);
+
+		sf::Vector2f getPosition();
+		float getRotationSpeed();
+		float getHeading();
+		void setPosition(sf::Vector2f position);
+		void setRotationSpeed(float speed);
+		void setHeading(float heading);
 	};
 }
 #endif
